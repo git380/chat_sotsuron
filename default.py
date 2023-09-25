f688df9426706600e762ee5cb531b31e54d79c64
git380 <108381952+git380@users.noreply.github.com>
2023-09-25 17:14:09 +0900

    defaultを、chat.htmlに対応

--- default.py
+++ default.py
@@ -13,18 +13,22 @@ def lambda_handler(event, context):
 
     # テーブルを取得
     table = dynamodb.Table('作成したDynamoDBの名前')
+    # テーブルから接続中のコネクションIDを取得
+    clients = table.scan(ProjectionExpression='id').get('Items')
 
     # eventのbody(送信されたチャットの内容)を取得
     message = event['body']
     # 受信したJSONデータをPythonオブジェクトに変換
     data = json.loads(message)
-    # dataオブジェクトのdataに送信内容が含まれる。
-    message_data = data.get('data')
-    print(message_data)
-    # テーブルから接続中のコネクションIDを取得
-    connection_ids = table.scan(ProjectionExpression='id').get('Items')
-    for connection_id in connection_ids:
-        print(connection_id)
+    # dataオブジェクトには'messageId', 'client_id', 'message'が含まれる
+    message_id = data.get('messageId', '')
+    client_id = data.get('client_id', '')
+    received_message = data.get('message', '')
+    checked = data.get('checked', False)  # チェック状態を取得
+    print(f"ID:{message_id}　受信ID：{client_id}　メッセージ:{received_message}　チェック状態:{checked}")
+    # クライアントからのメッセージをすべてのクライアントにブロードキャスト
+    for client in clients:
         # コネクションIDを指定してクライアントにデータをPOST
-        apigw.post_to_connection(ConnectionId=connection_id['id'], Data=message)
+        apigw.post_to_connection(ConnectionId=client['id'], Data=message)
+
     return {}
